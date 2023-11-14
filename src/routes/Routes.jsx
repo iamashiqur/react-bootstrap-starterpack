e08770cbd9d2bc374862869aa6799ad990aa2b47
muhammadashiqurrahman <getintouch.ashiq@gmail.com>
2023-11-14 15:17:35 +0600

    update routes

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,11 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/home/Home";
 import NotFound from "../pages/404/NotFound";
+import Layout from './../layout/Layout';
 
 export const router = createBrowserRouter([
     {
         path: '/',
-        element: <Home />,
-        errorElement: <NotFound />
+        element: <Layout />,
+        errorElement: <NotFound />,
+        children: [
+            {
+                path: '/',
+                element: <Home />
+            },
+        ]
     }
 ])
